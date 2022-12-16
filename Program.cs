62a1b9811c8d594737a3279adcefcbdf7c499275
Romaric <romaric.miaka.sio@gmail.com>
2022-12-16 10:33:30 -0800

    api endpoint

--- Program.cs
+++ Program.cs
@@ -6,18 +6,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<StaffMemberDB>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("StaffMember")));
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddScoped<IMemberService, MemberService>();
 
 var app = builder.Build();
 
-app.MapGet("/", ()=> "Hello World");
-app.MapGet("/members", (IMemberService memberService) => memberService.GetMembers());
-app.MapGet("/members/{id:int}", (int id, IMemberService memberService) => {
+app.MapGet("/api/", ()=> "Hello World");
+app.MapGet("/api/members", (IMemberService memberService) => memberService.GetMembers());
+app.MapGet("/api/members/{id:int}", (int id, IMemberService memberService) => {
     var member = memberService.GetMember(id);
-    return member == null ? Results.NotFound() : Results.Ok();
+    return member == null ? Results.NotFound() : Results.Ok(member);
 });
-app.MapPost("/members", (Member member, IMemberService memberService) => memberService.SaveMember(member));
-app.MapPut("/member", () => "Hello Millz!");
-app.MapDelete("/members/{id:int}", () => "Hello Millz!");
+app.MapPost("/api/members", (Member member, IMemberService memberService) => memberService.SaveMember(member));
+app.MapPut("/api/members{id:int}", (int id) => "Hello Millz!");
+app.MapDelete("/api/members/{id:int}", (int id) => "Hello Millz!");
 
 
 app.Run();
