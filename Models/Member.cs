92155f622c1b507011cbea618d40d5398d21657d
Romaric <romaric.miaka.sio@gmail.com>
2023-06-15 17:37:58 +0200

    nullable

--- Models/Member.cs
+++ Models/Member.cs
@@ -8,18 +8,18 @@ public class Member
     public int Id {get; set;}
 
     [StringLength(50)]
-    public string JobTitle {get; set;}
+    public string? JobTitle {get; set;}
     [StringLength(50)]
-    public string EmailAddress {get; set;}
+    public string? EmailAddress {get; set;}
     [StringLength(50)]
-    public string FirstName {get; set;}
+    public string? FirstName {get; set;}
     [StringLength(50)]
-    public string LastName {get; set;}
+    public string? LastName {get; set;}
     [StringLength(15)]
-    public string Gender {get; set;}
+    public string? Gender {get; set;}
     [StringLength(15)]
-    public string Phone {get; set;}
+    public string? Phone {get; set;}
     [StringLength(50)]
-    public string Country {get; set;}
+    public string? Country {get; set;}
     
 }
