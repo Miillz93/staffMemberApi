56481a2fe31a43a2009047b6b76ff3053408d7fe
Romaric <romaric.miaka.sio@gmail.com>
2023-06-15 17:32:54 +0200

    get members

--- Program.cs
+++ Program.cs
@@ -8,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<StaffMemberDB>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("StaffMember")));
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<ICsvMemberService, CsvMemberService>();
 
 var app = builder.Build();
 
 app.MapGet("/api/", ()=> "Hello World");
-app.MapGet("/api/members", (IMemberService memberService) => memberService.GetMembers());
+//app.MapGet("/api/members", (IMemberService memberService) => memberService.GetMembers());
+app.MapGet("/api/members", (ICsvMemberService memberService) => memberService.GetMembers());
+
 app.MapGet("/api/members/{id:int}", (int id, IMemberService memberService) => {
     var member = memberService.GetMember(id);
     return member == null ? Results.NotFound() : Results.Ok(member);
