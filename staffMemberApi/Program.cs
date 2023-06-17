f34cef9683b11c231c1612ace042831b92d463c6
Romaric <romaric.miaka.sio@gmail.com>
2023-06-17 20:48:27 +0200

    add endpoint

--- staffMemberApi/Program.cs
+++ staffMemberApi/Program.cs
@@ -1,5 +1,6 @@
 using FollowUpApi.Models;
 using FollowUpApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -14,13 +15,14 @@ var app = builder.Build();
 
 app.MapGet("/api/", ()=> "Hello World");
 //app.MapGet("/api/members", (IMemberService memberService) => memberService.GetMembers());
-app.MapGet("/api/members", (ICsvMemberService memberService) => memberService.GetMembers());
+app.MapGet("/api/members", (int pageNumber , int pageSize, ICsvMemberService memberService) => memberService.GetMembers(pageNumber, pageSize));
+app.MapGet("/api/members/{id:int}", (int id ,ICsvMemberService memberService) => memberService.GetMember(id));
 
-app.MapGet("/api/members/{id:int}", (int id, IMemberService memberService) => {
-    var member = memberService.GetMember(id);
-    return member == null ? Results.NotFound() : Results.Ok(member);
-});
-app.MapPost("/api/members", (Member member, IMemberService memberService) => memberService.SaveMember(member));
+// app.MapGet("/api/members/{id:int}", (int id, IMemberService memberService) => {
+//     var member = memberService.GetMember(id);
+//     return member == null ? Results.NotFound() : Results.Ok(member);
+// });
+// app.MapPost("/api/members", (Member member, IMemberService memberService) => memberService.SaveMember(member));
 app.MapPut("/api/members{id:int}", (int id) => "Hello Millz!");
 app.MapDelete("/api/members/{id:int}", (int id) => "Hello Millz!");
 
