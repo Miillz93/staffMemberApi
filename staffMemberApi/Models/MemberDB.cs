530fa989d156b4831317e9efe1e238763de9f354
Romaric <romaric.miaka.sio@gmail.com>
2023-06-17 12:28:30 +0200

    move files

--- staffMemberApi/Models/MemberDB.cs
+++ staffMemberApi/Models/MemberDB.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using StaffMemberApi.Models;
+
 namespace FollowUpApi.Models;
 
 public class StaffMemberDB : DbContext
 {
     //public DbSet<Member> Members {get;set;}
     public DbSet<Member> Members => Set<Member>();
-     public StaffMemberDB(DbContextOptions<StaffMemberDB> options)
+    public DbSet<User> Users => Set<User>();
+    public StaffMemberDB(DbContextOptions<StaffMemberDB> options)
         : base(options) { }
     
     
